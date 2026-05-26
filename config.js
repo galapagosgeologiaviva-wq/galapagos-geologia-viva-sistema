7ec00d7ad7bb33f18aab04dbdbebf324f1c9b5f4
galapagosgeologiaviva-wq <galapagosgeologiaviva@gmail.com>
2026-05-25 22:38:06 -0500

    Conectar carpetas Drive y configuración VEA

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const LINKS = {
-  // Falta pegar aquí el enlace de la carpeta madre general del sistema empresarial.
-  driveMadre: https://drive.google.com/drive/folders/1OgPKKFJyhwyC56HgjN9gSYhz5FO2qE8d?usp=drive_link,
+  // Carpeta madre general del sistema empresarial
+  driveMadre: 'https://drive.google.com/drive/folders/1OgPKKFJyhwyC56HgjN9gSYhz5FO2qE8d?usp=drive_link',
 
   // Carpetas principales en Drive
   gobierno: 'https://drive.google.com/drive/folders/10y9DdX9UZSby3Unmb0Xc14odiqZxmkko?usp=sharing',
@@ -19,21 +19,31 @@ const LINKS = {
   legal: 'https://drive.google.com/drive/folders/1sQSLH29iP5R8CDWc-nerPR4qA7ZKPZ5c?usp=drive_link',
   evidencias: 'https://drive.google.com/drive/folders/1mOtXPC_Qdky5-epKc9pOzk1PXbdfEKUn?usp=drive_link',
 
-  // Por ahora universidades apunta a la carpeta general de unidades de negocio.
+  // Por ahora universidades apunta a la carpeta general de unidades de negocio
   universidades: 'https://drive.google.com/drive/folders/1lAR-YxfSaTIB8vOt9O83iZ-JZq8R32-8?usp=drive_link',
 
-  // Pendientes de conexión posterior
+  // Pendientes para siguientes fases
   sheetsCRM: 'SHEETS_URL_CRM_AQUI',
   sheetsDashboard: 'SHEETS_URL_DASHBOARD_AQUI',
   formTarea: 'FORM_URL_REGISTRO_TAREA_AQUI',
   formLead: 'FORM_URL_REGISTRO_LEAD_AQUI'
 };
 
-function openLink(key){
+function openLink(key) {
   const url = LINKS[key];
-  if(!url || url.includes('_AQUI')){
+
+  if (!url || url.includes('_AQUI')) {
     alert('Enlace pendiente de configurar: ' + key);
     return;
   }
-  window.open(url,'_blank');
+
+  window.open(url, '_blank');
 }
+
+// Configuración específica para VEA semanal
+window.GGV_CONFIG = {
+  VEA_SHEET_URL: 'VEA_SHEET_URL_AQUI',
+  DRIVE_GOBIERNO_URL: 'https://drive.google.com/drive/folders/10y9DdX9UZSby3Unmb0Xc14odiqZxmkko?usp=sharing',
+  VEA_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwt-gKWSf9BeFwDhWbXFSq6w8SX75xUAlRgS7uB0xM2UpTK5ZV3SUKXoDr_RfCjk-T0/exec',
+  VEA_TOKEN: 'GGV2026'
+};
